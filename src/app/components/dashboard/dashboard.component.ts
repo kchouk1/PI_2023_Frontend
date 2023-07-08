6b92a3157b9fa7b530e49b9fec5e08fe9cbdf5a4
kchouk1 <ahmed.kchouk@esprit.tn>
2023-07-08 23:40:08 +0300

    start dashboard with fake data

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,339 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/_models/user';
+import { AppConfigService } from 'src/app/_services/AppConfigService';
+import { UserService } from 'src/app/_services/users.service';
+import { AppConfig } from 'src/app/layout/service/app.layout.service';
 
 @Component({
     templateUrl: './dashboard.component.html',
-    providers: [ConfirmationService, MessageService],
+    providers: [ConfirmationService, MessageService,AppConfigService],
 })
 export class DashboardComponent implements OnInit {
-    constructor() {}
+    basicData: any;
 
-    ngOnInit(): void {}
+    multiAxisData: any;
+
+    multiAxisOptions: any;
+
+    lineStylesData: any;
+
+    basicOptions: any;
+
+    subscription!: Subscription;
+    data: any;
+
+    chartOptions: any;
+    config!: AppConfig;
+    employees: User[] = [];
+
+    constructor(private messageService: MessageService, private configService: AppConfigService,  private userService: UserService) {}
+
+    ngOnInit() {
+        this.userService.getAllusers().subscribe((r) => {
+            this.employees = r;
+            console.log("kkkkkkkkkkkkkk",this.employees)
+        });
+        this.basicData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'Employees',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    borderColor: '#42A5F5',
+                    tension: .4
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderColor: '#FFA726',
+                    tension: .4
+                }
+            ]
+        };
+
+
+        this.data = {
+            labels: ['A','B','C'],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#42A5F5",
+                        "#66BB6A",
+                        "#FFA726"
+                    ],
+                    hoverBackgroundColor: [
+                        "#64B5F6",
+                        "#81C784",
+                        "#FFB74D"
+                    ]
+                }
+            ]
+        };
+
+        this.config = this.configService.config;
+       // this.updateChartOptions();
+        this.subscription = this.configService.configUpdate$.subscribe(config => {
+            this.config = config;
+         //   this.updateChartOptions();
+        });
+
+
+
+
+       
+
+        this.multiAxisData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Dataset 1',
+                fill: false,
+                borderColor: '#42A5F5',
+                yAxisID: 'y',
+                tension: .4,
+                data: [65, 59, 80, 81, 56, 55, 10]
+            }, {
+                label: 'Dataset 2',
+                fill: false,
+                borderColor: '#00bb7e',
+                yAxisID: 'y1',
+                tension: .4,
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }]
+        };
+
+        this.multiAxisOptions = {
+            stacked: false,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#495057'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        drawOnChartArea: false,
+                        color: '#ebedef'
+                    }
+                }
+            }
+        };
+
+        this.lineStylesData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    tension: .4,
+                    borderColor: '#42A5F5'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderDash: [5, 5],
+                    tension: .4,
+                    borderColor: '#66BB6A'
+                },
+                {
+                    label: 'Third Dataset',
+                    data: [12, 51, 62, 33, 21, 62, 45],
+                    fill: true,
+                    borderColor: '#FFA726',
+                    tension: .4,
+                    backgroundColor: 'rgba(255,167,38,0.2)'
+                }
+            ]
+        };
+
+        this.config = this.configService.config;
+     //   this.updateChartOptions();
+        this.subscription = this.configService.configUpdate$.subscribe(config => {
+            this.config = config;
+       //     this.updateChartOptions();
+        });
+    }
+
+
+
+    applyLightTheme() {
+        this.basicOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#495057'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                }
+            }
+        };
+
+        this.multiAxisOptions = {
+            stacked: false,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#495057'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        drawOnChartArea: false,
+                        color: '#ebedef'
+                    }
+                }
+            }
+        };
+    }
+
+    applyDarkTheme() {
+        this.basicOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#ebedef'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#ebedef'
+                    },
+                    grid: {
+                        color: 'rgba(255,255,255,0.2)'
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: '#ebedef'
+                    },
+                    grid: {
+                        color: 'rgba(255,255,255,0.2)'
+                    }
+                }
+            }
+        };
+
+        this.multiAxisOptions = {
+            stacked: false,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#ebedef'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#ebedef'
+                    },
+                    grid: {
+                        color: 'rgba(255,255,255,0.2)'
+                    }
+                },
+                y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    ticks: {
+                        color: '#ebedef'
+                    },
+                    grid: {
+                        color: 'rgba(255,255,255,0.2)'
+                    }
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    ticks: {
+                        color: '#ebedef'
+                    },
+                    grid: {
+                        drawOnChartArea: false,
+                        color: 'rgba(255,255,255,0.2)'
+                    }
+                }
+            }
+        };
+    }
 }
