4c78a839a7aa5da5ea44ae3d95df14af8a1f8f4e
kchouk1 <ahmed.kchouk@esprit.tn>
2023-07-09 18:05:45 +0300

    fix exportexcell

--- src/app/components/contenu/utilisateurs/utilisateurs.component.ts
+++ src/app/components/contenu/utilisateurs/utilisateurs.component.ts
@@ -27,7 +27,7 @@ export class UtilisateursComponent implements OnInit {
     isAdmin: boolean = false;
     exportColumns: any[] | undefined;
     cols: any[] | undefined;
-    countusers: number=0;
+    countusers: number = 0;
 
     @ViewChild('dt') dt: Table | undefined;
     CongeService: any;
@@ -44,16 +44,17 @@ export class UtilisateursComponent implements OnInit {
         this.getAll();
         this.userService.getUserCount().subscribe((r: number) => {
             this.countusers = r;
-           
         });
         this.cols = [
-            { field: 'username', header: 'User Name', customExportHeader: 'username' },    
+            {
+                field: 'username',
+                header: 'User Name',
+                customExportHeader: 'username',
+            },
             { field: 'email', header: 'Email' },
             { field: 'firstName', header: 'First Name' },
             { field: 'lastName', header: 'Last Name' },
             { field: 'phoneNumber', header: 'Phone Number' },
-
-        
         ];
 
         this.exportColumns = this.cols.map((col) => ({
@@ -81,65 +82,60 @@ export class UtilisateursComponent implements OnInit {
         this.userDialog = true;
     }
 
-   ExportPDF() {
-    console.log("kkkkkkkkkkkkk",this.countusers);
-    let doc = new jsPDF.default('l', 'pt');
-    var img = new Image();
-    img.src = 'assets/images/HR1.png';  
-    doc.addImage(img, 'png', 100, 20, 100, 100);
-    doc.setTextColor(0, 0, 139);
-    var date = formatDate(new Date(), 'yyyy/MM/dd hh:mm a', 'en');
-    doc.text(600, 70, 'HR.TN');
-    doc.text(600, 90, ' ' + date);
-    doc.text(600, 110, 'Total des Utilisateurs est ' + this.countusers);
-    doc.text(110, 110, 'HR.TN');
-    doc.setTextColor(255, 0, 0);
-    doc.text(320, 130, 'Liste des Utilisateurs\n');
-    doc.autoTable(this.exportColumns, this.users, {
-        theme: 'grid',
-        styles: {
-            halign: 'left',
-        },
-        margin: {
-            top: 180,
-        },
-    });
-    doc.setTextColor(0, 0, 0);
-    doc.save('Utilisateurs_' + new Date().getTime() + '.pdf');
-}
+    ExportPDF() {
+        console.log('kkkkkkkkkkkkk', this.countusers);
+        let doc = new jsPDF.default('l', 'pt');
+        var img = new Image();
+        img.src = 'assets/images/HR1.png';
+        doc.addImage(img, 'png', 100, 20, 100, 100);
+        doc.setTextColor(0, 0, 139);
+        var date = formatDate(new Date(), 'yyyy/MM/dd hh:mm a', 'en');
+        doc.text(600, 70, 'HR.TN');
+        doc.text(600, 90, ' ' + date);
+        doc.text(600, 110, 'Total des Utilisateurs est ' + this.countusers);
+        doc.text(110, 110, 'HR.TN');
+        doc.setTextColor(255, 0, 0);
+        doc.text(320, 130, 'Liste des Utilisateurs\n');
+        doc.autoTable(this.exportColumns, this.users, {
+            theme: 'grid',
+            styles: {
+                halign: 'left',
+            },
+            margin: {
+                top: 180,
+            },
+        });
+        doc.setTextColor(0, 0, 0);
+        doc.save('Utilisateurs_' + new Date().getTime() + '.pdf');
+    }
 
+    exportexcell(exportColumns: any) {
+        const replacer = (key: any, value: null) =>
+            value === null ? '' : value;
+        const header = Object.keys(exportColumns[0]);
+        let csv = exportColumns.map((row: { [x: string]: any }) =>
+            header
+                .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+                .join(',')
+        );
+        csv.unshift(header.join(','));
+        let csvArray = csv.join('\r\n');
+        var blob = new Blob([csvArray], { type: 'text/csv' });
+        saveAs(blob, 'Utilisateurs_' + new Date().getTime() + '.csv');
+    }
 
-exportexcell(exportColumns: any) {
-    const replacer = (key: any, value: null) =>
-        value === null ? '' : value; 
-    const header = Object.keys(exportColumns[0]);
-    let csv = exportColumns.map((row: { [x: string]: any }) =>
-        header
-            .map((fieldName) => JSON.stringify(row[fieldName], replacer))
-            .join(',')
-    );
-    csv.unshift(header.join(','));
-    let csvArray = csv.join('\r\n');
-    var blob = new Blob([csvArray], { type: 'text/csv' });
-    saveAs(blob, 'Utilisateurs_' + new Date().getTime() + '.csv');
-}
-
-
-
-savecsv(buffer: any, fileName: string): void {
-    let EXCEL_TYPE =
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    let EXCEL_EXTENSION = '.xlsx';
-    const data: Blob = new Blob([buffer], {
-        type: EXCEL_TYPE,
-    });
-    FileSaver.saveAs(
-        data,
-        fileName + '_' + new Date().getTime() + EXCEL_EXTENSION
-    );
-}
-
-
+    savecsv(buffer: any, fileName: string): void {
+        let EXCEL_TYPE =
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const data: Blob = new Blob([buffer], {
+            type: EXCEL_TYPE,
+        });
+        FileSaver.saveAs(
+            data,
+            fileName + '_' + new Date().getTime() + EXCEL_EXTENSION
+        );
+    }
 
     deleteSelectedUsers() {
         this.confirmationService.confirm({
